72f1f8f87c85f0927376d95999010d48722fb11d
Alexander Chekmenev <alexchekmenev@gmail.com>
2023-11-04 10:54:55 +0300

    add TelegramMainButton

--- components/CreateNewLink/index.tsx
+++ components/CreateNewLink/index.tsx
@@ -9,6 +9,7 @@ import { DurationSelector } from './DurationSelector'
 import { ScheduleComponent } from './ScheduleComponent'
 import { TimezoneSelectorComponent } from '@/components/TimezoneSelectorComponent'
 import { LinkIdPicker } from '@/components/CreateNewLink/LinkIdPicker'
+import { TelegramMainButton } from '@/components/TelegramMainButton'
 
 export function CreateNewLinkComponent() {
   function getDefaultTimezone() {
@@ -24,50 +25,54 @@ export function CreateNewLinkComponent() {
   const userId = 53698235 // TODO pass tgUserId
 
   return (
-    <div className="grid grid-flow-row sm:max-w-sm xs:max-w-xs">
-      <div className="flex-1">
-        <TimezoneSelectorComponent
-          timezone={timezone}
-          onTimezoneChanged={setTimezone}
-        />
-      </div>
-
-      <div className="flex-1">
-        <ScheduleComponent
-          schedule={schedule}
-          onScheduleUpdated={setSchedule}
-        />
-      </div>
-
-      <div className="flex-1">
-        <DurationSelector
-          duration={duration}
-          onDurationSelected={setDuration}
-        />
-      </div>
-
-      {duration && (
-        <p className="flex-1 text-red-600">
-          Selected slot duration: {duration.hours}h {duration.minutes}m
-        </p>
-      )}
-
-      <div className="flex-1">
-        <LinkIdPicker />
-      </div>
-
-      <div className="flex-1">
-        {duration && schedule && linkId && (
-          <CreateNewLinkForm
-            linkId={linkId}
-            userId={userId}
+    <>
+      <div className="grid grid-flow-row sm:max-w-sm xs:max-w-xs">
+        <div className="flex-1">
+          <TimezoneSelectorComponent
             timezone={timezone}
-            duration={duration}
-            schedule={schedule}
-            onLinkCreated={() => {}}
+            onTimezoneChanged={setTimezone}
           />
+        </div>
+
+        <div className="flex-1">
+          <ScheduleComponent
+            schedule={schedule}
+            onScheduleUpdated={setSchedule}
+          />
+        </div>
+
+        <div className="flex-1">
+          <DurationSelector
+            duration={duration}
+            onDurationSelected={setDuration}
+          />
+        </div>
+
+        {duration && (
+          <p className="flex-1 text-red-600">
+            Selected slot duration: {duration.hours}h {duration.minutes}m
+          </p>
         )}
+
+        <div className="flex-1">
+          <LinkIdPicker />
+        </div>
+
+        <div className="flex-1">
+          {duration && schedule && linkId && (
+            <CreateNewLinkForm
+              linkId={linkId}
+              userId={userId}
+              timezone={timezone}
+              duration={duration}
+              schedule={schedule}
+              onLinkCreated={() => {}}
+            />
+          )}
+        </div>
       </div>
-    </div>
+
+      <TelegramMainButton />
+    </>
   )
 }
---
+++ components/TelegramMainButton.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { MainButton } from '@twa-dev/sdk/dist/react'
+
+export function TelegramMainButton() {
+  const [alertShown, setAlertShown] = React.useState<boolean>(false)
+
+  useEffect(function mount() {
+    setAlertShown(true)
+    // function onScroll() {
+    //   console.log('scroll!')
+    // }
+    //
+    // window.addEventListener('scroll', onScroll)
+    //
+    // return function unMount() {
+    //   window.removeEventListener('scroll', onScroll)
+    // }
+  }, [])
+
+  return (
+    <>
+      {alertShown && (
+        <MainButton text="Submit" onClick={() => alert('submitted')} />
+      )}
+    </>
+  )
+}
