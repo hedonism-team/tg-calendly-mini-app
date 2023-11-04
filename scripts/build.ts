3bfa420d8db1c4c4d563e2427ae25556ef35742f
Alexander Chekmenev <alexchekmenev@gmail.com>
2023-11-04 18:19:05 +0300

    fix import (3)

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,9 +1,14 @@
-import { bot } from '../lib/bot'
+import { Bot } from 'grammy'
 
 const {
   VERCEL_URL: host,
   // set your webhook address or use default Vercel deployment url
   WEBHOOK: webhook = `https://${host}/api/webhook`,
 } = process.env
+
+const { BOT_TOKEN: token = '' } = process.env
+
+// Set your token in the vercel environment variable
+export const bot = new Bot(token)
 
 void bot.api.setWebhook(webhook)
