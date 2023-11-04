7ae7af1d8c6f3af06fc69efcd5f952edd7018c85
Alexander Chekmenev <alexchekmenev@gmail.com>
2023-11-04 16:14:45 +0300

    add set handler

--- app/api/bot.ts
+++ app/api/bot.ts
@@ -1,13 +1,25 @@
 import { Bot, webhookCallback } from 'grammy'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-const bot = new Bot(String(process.env.BOT_TOKEN))
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const bot = new Bot(String(process.env.BOT_TOKEN))
+  console.log("Initialized instance successfully");
 
-// Handle the /start command.
-bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
-// Handle other messages.
-bot.on('message', (ctx) => ctx.reply('Got another message!'))
+  // Handle the /start command.
+  bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
+  // Handle other messages.
+  bot.on('message', (ctx) => ctx.reply('Got another message!'))
 
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
+  // Now that you specified how to handle messages, you can start your bot.
+  // This will connect to the Telegram servers and wait for messages.
 
-export default webhookCallback(bot, 'next-js')
+  // Create a request handler
+  const handle = webhookCallback(bot, 'next-js')
+  console.log('Created handler successfully')
+
+  // Return the handler to Vercel api
+  return await handle(req, res)
+}
