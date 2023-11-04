5f7097b499d3301dc00c89e8cc7109961baf269a
Alexander Chekmenev <alexchekmenev@gmail.com>
2023-11-04 16:47:13 +0300

    catch all routes handler

--- app/api/bot/set/route.ts
+++ app/api/bot/set/route.ts
@@ -4,11 +4,12 @@ export async function GET() {
   const bot = new Bot(String(process.env.BOT_TOKEN))
   console.log('Initialized instance successfully')
 
-  const address: string = 'https://tg-calendly-mini-app.vercel.app/api/bot'
+  const address: string =
+    'https://tg-calendly-mini-app.vercel.app/api/bot/webhook'
 
   try {
-    await bot.api.setWebhook(address)
-    return Response.json({ status: 'Done. Set!' })
+    const result = await bot.api.setWebhook(address)
+    return Response.json({ status: 'Done. Set!', result })
   } catch (_) {
     return Response.json(
       { status: "Couldn't succeed with installing webhook!" },
