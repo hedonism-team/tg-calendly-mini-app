f4f0a5973a36d8774f23e9829b56913bd944508b
Alexander Chekmenev <alexchekmenev@gmail.com>
2023-11-04 18:17:15 +0300

    fix import (2)

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,4 +1,4 @@
-import { bot } from '../../../lib/bot'
+import { bot } from '@/lib/bot'
 import { webhookCallback } from 'grammy'
 
 // webhookCallback will make sure that the correct middleware(listener) function is called
