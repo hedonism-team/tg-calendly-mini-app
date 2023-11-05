f2ae7b3328af4446c00cd263bad1b47db1fa9368
Alexander Chekmenev <alexchekmenev@gmail.com>
2023-11-05 20:50:13 +0300

    fix success screens (3)

--- components/CreateNewAppointment/AppointmentCreationSuccess.tsx
+++ components/CreateNewAppointment/AppointmentCreationSuccess.tsx
@@ -6,8 +6,8 @@ export function AppointmentCreationSuccess() {
       <div className="w-80 h-fit m-auto text-center">
         <h1 className="font-bold">🎉 Success!</h1>
         <h2>
-          Your appointment request has been successfully sent. I get a
-          notification via bot once the request is resolved
+          Your appointment request has been successfully sent. You get a
+          notification via bot once the request is resolved.
         </h2>
       </div>
     </div>
