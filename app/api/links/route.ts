b8208fab0d1c8e9351dca870e02c598dc759d4cd
Alexander Chekmenev <alexchekmenev@gmail.com>
2023-11-05 23:33:14 +0300

    adjust notifications

--- app/api/links/route.ts
+++ app/api/links/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { LinkModel } from '@/lib/models/Link.model'
 import { createOrUpdateLink } from '@/lib/services/links.service'
 import { ScheduleModel } from '@/lib/models/Schedule.model'
+import { sendLinkCreatedNotification } from '@/lib/services/notifications.service'
 
 export interface CreateNewLinkPayload {
   link: Omit<LinkModel, 'schedule' | 'id'>
@@ -13,6 +14,7 @@ export async function POST(request: NextRequest) {
   const data = await request.json()
   try {
     const { link, isNew } = await createOrUpdateLink(data.link, data.schedule)
+    await sendLinkCreatedNotification(link)
     return NextResponse.json({ link }, { status: isNew ? 201 : 200 })
   } catch (e) {
     const error = e as Error
--- lib/services/notifications.service.ts
+++ lib/services/notifications.service.ts
@@ -9,14 +9,21 @@ import {
   getAppointmentById,
   updateAppointmentStatus,
 } from '@/lib/services/appointments.service'
+import { UserModel } from '@/lib/models/User.model'
+import { LinkModel } from '@/lib/models/Link.model'
+import { getUserById } from '@/lib/services/users.service'
 
 export async function sendNewAppointmentNotification({
   id: appointmentId,
   userId,
   requestingUserId,
+  date,
+  timeSlot,
 }: AppointmentModel) {
-  // TODO add clickable link to Tg profile
-  const responseText = `You've got a new appointment from User#${requestingUserId}`
+  const requestingUser = await getUserById(requestingUserId)
+  const responseText = `You've got a new appointment from ${getUserNickname(
+    requestingUser!
+  )}. Details: ${date} at ${timeSlot.startTime}`
   const inlineKeyboard = new InlineKeyboard()
     .text('❌ Reject', `${AppointmentStatus.rejected}|${appointmentId}`)
     .text('✅ Approve', `${AppointmentStatus.approved}|${appointmentId}`)
@@ -38,10 +45,14 @@ export async function updateNewAppointmentNotification({
   chatId: number
   messageId: number
 }) {
-  const { requestingUserId } = await getAppointmentById(appointmentId)
+  const { requestingUserId, date, timeSlot } = await getAppointmentById(
+    appointmentId
+  )
+  const requestingUser = await getUserById(requestingUserId)
   await updateAppointmentStatus(appointmentId, appointmentStatus)
-  // TODO add clickable link to Tg profile
-  const responseText = `You've ${appointmentStatus} an upcoming appointment with User#${requestingUserId}`
+  const responseText = `You've ${appointmentStatus} an upcoming appointment with ${getUserNickname(
+    requestingUser!
+  )}. Details: ${date} at ${timeSlot.startTime}`
   await bot.api.editMessageText(chatId, messageId, responseText)
 }
 
@@ -50,9 +61,49 @@ export async function sendAppointmentRequestResultNotification(
   appointmentStatus: AppointmentStatus
 ) {
   const { userId, requestingUserId } = await getAppointmentById(appointmentId)
-  // TODO add clickable link to Tg profile
-  const responseText = `Your request has been ${appointmentStatus} by User#${userId}`
+  const hostUser = await getUserById(userId)
+  const responseText = `Your request has been ${appointmentStatus} by ${getUserNickname(
+    hostUser!
+  )}`
   const result = await bot.api.sendMessage(requestingUserId, responseText)
   console.log('[AppointmentRequestResultNotification] result', result)
   return result
+}
+
+export async function sendLinkCreatedNotification({ id, userId }: LinkModel) {
+  const mainMessageText = `<b>🎉 Your link created!</b>\n\nUse this link to receive incoming booking requests\n\n<code>${getFullLink(
+    id
+  )}</code>\n\nShare the message below to start getting bookings`
+  const mainMessage = await bot.api.sendMessage(userId, mainMessageText, {
+    parse_mode: 'HTML',
+  })
+  console.log('[LinkCreatedNotification] mainMessage', mainMessage)
+
+  const sharableMessageText = `Book my time by clicking <a href="${getFullLink(
+    id
+  )}">here</a>`
+  const sharableMessage = await bot.api.sendMessage(
+    userId,
+    sharableMessageText,
+    {
+      parse_mode: 'HTML',
+    }
+  )
+  console.log('[LinkCreatedNotification] sharableMessage', sharableMessage)
+}
+
+// helpers
+
+function getFullLink(id: LinkModel['id']) {
+  return `https://t.me/meetly_bot/app?startapp=l_${id}`
+}
+
+export function getUserNickname(user: UserModel) {
+  let response
+  if (user.username) {
+    response = `@${user.username}`
+  } else {
+    response = `${user.firstName ?? ''} ${user.lastName ?? ''}`
+  }
+  return response
 }
