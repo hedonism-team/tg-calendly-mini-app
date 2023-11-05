6cece8891529f600660110b452e4bc10869fcb1a
Alexander Chekmenev <alexchekmenev@gmail.com>
2023-11-06 02:01:52 +0300

    remove eruda

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Hello Meetly!</title>
+        <title>Meetly — a Telegram bot for time booking</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
-        <script id="eruda-init">eruda.init();</script>
         <script src="https://telegram.org/js/telegram-web-app.js"></script>
       </head>
       <body>
